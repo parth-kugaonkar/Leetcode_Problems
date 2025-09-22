373d538e8b711c0447147b7cf30a642904a92350
Parth Kugaonkar <107021236+parth-kugaonkar@users.noreply.github.com>
2025-09-22 20:50:20 +0530

    Update Leetcode49.py

--- Leetcode49.py
+++ Leetcode49.py
@@ -1,5 +1,9 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# *The idea is to group words that are anagrams, meaning they’re made up of the same letters but arranged differently, like “eat”, “tea”, and “ate”. To do this, we use a dictionary where each key represents a kind of “signature” for the word, and the value is a list of all words that match that signature.
+# *The signature we use is just the sorted version of the word because sorting the letters makes all anagrams look the same. For example, “eat”, “tea”, and “ate” all become “aet” when sorted. So, we map “aet” to the list ["eat", "tea", "ate"].
+# *We repeat this for every word in the list: sort the word, use the sorted result as the key, and then add the word to the appropriate group in the dictionary.
+# *At the end, the dictionary values give us the groups of anagrams, which we simply return.
+
+
 >>> class Solution:
 ...     def groupAnagrams(self, strs):
 ...         anagram_map = defaultdict(list)
@@ -8,3 +12,4 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ...             sorted_word = ''.join(sorted(word))
 ...             anagram_map[sorted_word].append(word)
 ...         
+
